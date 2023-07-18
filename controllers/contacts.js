bb1568cba07c98bb97fb87fc78a80f47d7a6d75f
evgeniabr <113677474+evgeniabr@users.noreply.github.com>
2023-07-18 09:22:54 +0300

    hw - 3

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,18 +1,13 @@
 const { HttpError, ctrlWrapper } = require("../helpers");
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 
 const listContacts = async (req, res) => {
-  const result = await contacts.listContacts();
-  res.json({
-    status: "success",
-    code: 200,
-    result,
-  });
+  const result = await Contact.find();
+  res.json(result);
 };
-
 const getContactById = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.getContactById(id);
+  const result = await Contact.findById(id);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -24,7 +19,7 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.json({
     status: "success",
     code: 201,
@@ -34,7 +29,7 @@ const addContact = async (req, res) => {
 
 const removeContact = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.removeContact(id);
+  const result = await Contact.findByIdAndRemove(id);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -45,7 +40,20 @@ const removeContact = async (req, res) => {
 
 const updateContact = async (req, res) => {
   const { id } = req.params;
-  const result = await contacts.updateContact(id, req.body);
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    result,
+  });
+};
+
+const updateFavorite = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -62,4 +70,5 @@ module.exports = {
   addContact: ctrlWrapper(addContact),
   removeContact: ctrlWrapper(removeContact),
   updateContact: ctrlWrapper(updateContact),
+  updateFavorite: ctrlWrapper(updateFavorite),
 };
